700c199806b2fc8504d9a11b031850aba7fc46aa
Himanshu <himgamer467@gmail.com>
2026-03-05 00:33:26 +0530

    stake.js

---
+++ stake.js
@@ -0,0 +1,17 @@
+export default function Stake() {
+  return (
+    <div>
+      <h2>Stake Your NFT</h2>
+
+      <div className="card">
+        <p>NFT Collection</p>
+        <button>Stake NFT</button>
+      </div>
+
+      <div className="card">
+        <p>Your Rewards</p>
+        <button>Claim Yield</button>
+      </div>
+    </div>
+  );
+}
